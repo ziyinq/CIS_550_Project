5409eab4ec6b0730aedce42c3bb8ed1ba4c311a6
ziyin <roshomon.vol@gmail.com>
2017-12-07 22:07:08 -0500

    update data

--- pyconvert/entity/songAttributes_convert.py
+++ pyconvert/entity/songAttributes_convert.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 df = pd.DataFrame()
-df = pd.read_csv('../data/songAttributes.csv')
+df = pd.read_csv('../../data/songAttributes.csv')
+df2 = pd.read_csv('../../data/billboards.csv')
 df = df[['id','acousticness','danceability','duration_ms','energy','instrumentalness','key','liveness','loudness','mode','speechiness','tempo','time_signature','valence']]
+df2 = df2[['Song','Year']]
+df['Song']=df2['Song']
+df['Year']=df2['Year']
+print(df)
 #df = df.rename(columns={'album_id': 'id', 'album_name':'name', 'album_type':'type'})
-df.to_csv('songAttributes_converted.csv', index=False, encoding='utf-8', header=True)
+df.to_csv('songAttributes.csv', index=False, encoding='utf-8', header=True)
